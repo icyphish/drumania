85e485474f31b9784deed245cd808ed8224671cd
Peter <54757580+icyphish@users.noreply.github.com>
2023-09-26 02:03:39 +1000

    Add files via upload

--- Drum Mania/index.js
+++ Drum Mania/index.js
@@ -1,56 +1,54 @@
 var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
-for (var i = 0; i < numberOfDrumButtons; i++)
-{
-    document.querySelectorAll(".drum")[i].addEventListener("click", function()
-    {
+for (var i = 0; i < numberOfDrumButtons; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+        var buttonClicked = this.innerHTML;
+        makeSound(buttonClicked);
+    });
+}
 
-    var buttonClicked = this.innerHTML;
+document.addEventListener("keypress", function(event) {
+    makeSound(event.key);
+});
 
-    switch (buttonClicked) {
+function makeSound(key) {
+    switch (key) {
         case "w":
             var tom1 = new Audio("sounds/tom-1.mp3")
-            tom1.play();         
-        break;
+            tom1.play();
+            break;
 
         case "a":
             var tom2 = new Audio("sounds/tom-2.mp3")
-            tom2.play();         
-        break;
+            tom2.play();
+            break;
 
         case "s":
             var tom3 = new Audio("sounds/tom-3.mp3")
-            tom3.play();         
-        break;
+            tom3.play();
+            break;
 
         case "d":
             var tom4 = new Audio("sounds/tom-4.mp3")
-            tom4.play();         
-        break;
+            tom4.play();
+            break;
 
         case "j":
             var snare = new Audio("sounds/snare.mp3")
             snare.play();
-        break;
+            break;
 
         case "k":
             var crash = new Audio("sounds/crash.mp3")
             crash.play();
-        break;
+            break;
 
         case "l":
             var kick = new Audio("sounds/kick-bass.mp3")
             kick.play();
-        break;
+            break;
 
-        default: alert(buttonClicked + " is not currently supported.")
-
+        default:
+            alert(key + " is not currently supported.");
     }
-
-        
-
-    });
 }
-
-//var audio = new Audio("sounds/tom-1.mp3");
-//audio.play();
